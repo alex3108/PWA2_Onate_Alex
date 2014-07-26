4e2fbf2c10902bf2da3a469fb842db2bc623881c
Alex Onate <alexonate@gmail.com>
2014-07-25 20:48:02 -0400

    zip code lookup - upload

--- Wk2/js/main.js
+++ Wk2/js/main.js
@@ -136,8 +136,31 @@ $('.mystatus').mouseover(function() {
 
 $('.mystatus').mouseover(function() {
 	$(this).fadeTo(100, .1);
-});
-});
+
+
+
+/*
+	===============================================
+    Tabbed Accordion for projects========================= 
+	*/
+	
+$('#tabs p').hide().eq(0).show();
+$('#tabs p:not(:first)').hide();
+
+$('#tabs-nav li').click(function(e) {
+     e.preventDefault();
+     $('#tabs p').hide();
+
+$('#tabs-nav .current').removeClass("current");
+   $(this).addClass('current');
+   var clicked = $(this).find('a:first').attr('href');
+   
+   $('#tabs ' + clicked).fadeIn('fast');
+}).eq(0).addClass('current');
+
+
+
+ 
 
  
 	
